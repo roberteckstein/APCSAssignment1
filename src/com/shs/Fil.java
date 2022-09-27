04ae210047d021ee5b37f637dce476dd4d36ff88
Archnitest <derekxl24@sherwoodsd.com>
2022-09-27 12:33:49 -0700

    fun stuff

---
+++ src/com/shs/Fil.java
@@ -0,0 +1,27 @@
+package com.shs;
+
+public class Fil {
+    int a; int b; int c; int d; int e; int f; int g; String h; String i; String j; String k; String l; String m; String n; double o; double p;
+
+    public Fil (int a, int b, int c, int d, int e, int f, int g, String h, String i, String j, String k, String l, String m, String n, double o, double p) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.e = e;
+        this.f = f;
+        this.g = g;
+        this.h = h;
+        this.i = i;
+        this.j = j;
+        this.k = k;
+        this.l = l;
+        this.m = m;
+        this.n = n;
+        this.o = o;
+        this.p = p;
+    }
+    private void print() {
+        System.out.println(a + b + c + d + e + f + g + h + i + j + k + l + m + n + o + p);
+    }
+}
