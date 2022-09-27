5a0ec86369981a29cf7d84dfe09c59d8f587e561
Tyler <tylerkn23@sherwoodsd.com>
2022-09-27 12:58:09 -0700

    commit

--- src/com/shs/CyrusHo.java
+++ src/com/shs/CyrusHo.java
@@ -1,18 +1,42 @@
 package com.shs;
 
 public class CyrusHo {
-    private String getAnime;
+    private String Anime;
     private int hoursSlept;
     private int passingClasses;
 
-    public void CyrusHo(String getAnime, int hoursSlept, int passingClasses) {
-        this.getAnime = getAnime;
+    public void CyrusHo(String Anime, int hoursSlept, int passingClasses) {
+        this.Anime = Anime;
         this.hoursSlept = hoursSlept;
         this.passingClasses = passingClasses;
     }
 
+    public String getAnime() {
+        return(this.Anime);
+    }
+
+    public void setAnime(String Anime) {
+        this.Anime = Anime;
+    }
+
+    public int getHoursSlept() {
+        return(this.hoursSlept);
+    }
+
+    public void setHoursSlept(int hoursSlept) {
+        this.hoursSlept = hoursSlept;
+    }
+
+    public int getPassingClasses() {
+        return(this.passingClasses);
+    }
+
+    public void setPassingClasses(int passingClasses) {
+        this.passingClasses = passingClasses;
+    }
+
     public void CyrusHo() {
-        getAnime = "Overflow";
+        Anime = "Overflow";
         hoursSlept = 10000000;
         passingClasses = 0;
     }
